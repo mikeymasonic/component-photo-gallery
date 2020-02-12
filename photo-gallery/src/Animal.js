463bd28f70e9f4d4dd162242a3ce409bef279514
Michael Romay <amerror@gmail.com>
2020-02-11 23:38:06 -0800

    did the things

---
+++ photo-gallery/src/Animal.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+export default class Animal extends Component {
+    render() {
+        return <li>
+            <div className="info-container">
+                <h2>{this.props.animal.keyword}</h2>
+                <p className="animal-title">{this.props.animal.title}</p>
+            </div>
+
+            <div className="image-container">
+                <img
+                    alt={this.props.animal.url}
+                    src={this.props.animal.url} />
+            </div>
+            <p className="horns-class">Horns: {this.props.animal.horns}</p>
+        </li>
+    }
+}
--- photo-gallery/src/App.js
+++ photo-gallery/src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Animal from "./Animal";
+import animalData from "./data.js";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = { selected: null };
+
+  render() {
+    const animalNodes = animalData
+      .filter(animal => {
+        if (!this.state.selected) return true;
+
+        return animal.horns === this.state.selected || animal.keyword === this.state.selected;
+      })
+      .map(animal => <Animal animal={animal} />);
+
+    const handleChange = e => {
+      this.setState({ selected: e.target.value });
+    };
+
+    return (
+      <div>
+        <header>
+          <img class="logo" src="logo.png" alt="Horn" />
+          <h1>Various Horned Animals</h1>
+        </header>
+
+        <main>
+
+        <section className="options1">
+            <select className="animal-keyword-filter" onChange={handleChange}>
+              <option value="" defaultValue>
+                Keyword
+              </option>
+              <option value="narwhal">narwhal</option>
+              <option value="rhino">rhino</option>
+              <option value="unicorn">unicorn</option>
+              <option value="unilego">unilego</option>
+            </select>
+          </section>
+
+
+          <section className="options">
+            <select className="animal-horn-filter" onChange={handleChange}>
+              <option value="" defaultValue>
+                # of Horns
+              </option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3 </option>
+              <option value="100">100</option>
+            </select>
+          </section>
+
+          <section className="list-section">
+            <ul className="animals">{animalNodes}</ul>
+          </section>
+        </main>
+      </div>
+    );
+  }
 }
-
-export default App;
